102d8d66dd0a5bd86a83c718c6cbd2d6c4823fed
Alex Udodov <alex.udodov.web@gmail.com>
2023-06-25 01:36:52 +0300

    style fixes

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,18 +1,17 @@
 
 import SearchBar from "../SearchBar/SearchBar";
 import HandleSearchFn from "../../pages/WeatherPage/HandleSearchFn";
-
 import Logo from "../Logo/Logo";
 
-
+import './NavBar.css';
 
 const NavBar: React.FC<HandleSearchFn> = ({ handleSearch }) => {
 
     return(
-        <>
+        <div className="nav-bar">
             <Logo />
             <SearchBar handleSearch={handleSearch} />
-        </>
+        </div>
     )
 };
 
