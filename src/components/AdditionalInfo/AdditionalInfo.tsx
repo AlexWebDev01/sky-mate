afc7b1f100222ef4805d2cd982ba93d6d202aa4b
Alex Udodov <alex.udodov.web@gmail.com>
2023-08-03 22:35:20 +0300

    refactor: refactored AdditionalInfo component

--- src/components/AdditionalInfo/AdditionalInfo.tsx
+++ src/components/AdditionalInfo/AdditionalInfo.tsx
@@ -3,28 +3,26 @@ import './AdditionalInfo.css';
 import { epochToLocalTime } from "../../helpers";
 
 const AdditionalInfo = ({ weatherData }: any) => {
+  const additionalInfoContent = [
+    {title: 'sunrise', value: epochToLocalTime(weatherData.current.sunrise, weatherData.timezone_offset)},
+    {title: 'sunset', value: epochToLocalTime(weatherData.current.sunset, weatherData.timezone_offset)},
+    {title: 'wind speed', value: `${Math.round(weatherData.current.wind_speed * 10) / 10} m/s`},
+    {title: 'pressure', value: `${weatherData.current.pressure} m/s`}
+  ];
   const pageStyle = weatherData.daily[0].weather[0].main.toLowerCase();
 
-    return (
-        <div className="additional-info">
-        <div>
-          <div className={`${pageStyle} title`}>sunrise</div>
-          <div className="value">{epochToLocalTime(weatherData.current.sunrise, weatherData.timezone_offset)}</div>
-        </div>
-        <div>
-          <div className={`${pageStyle} title`}>sunset</div>
-          <div className="value">{epochToLocalTime(weatherData.current.sunset, weatherData.timezone_offset)}</div>
-        </div>
-        <div>
-            <div className={`${pageStyle} title`}>wind speed</div>
-            <div className="value">{Math.round(weatherData.current.wind_speed * 10) / 10} m/s</div>
-        </div>
-        <div>
-            <div className={`${pageStyle} title`}>pressure</div>
-            <div className="value">{weatherData.current.pressure} m/s</div>
-        </div>
-      </div>
-    )
+  return(
+    <div className="additional-info">
+      {additionalInfoContent.map((item: any) => {
+          return (
+              <div key='index'>
+                <div className={`${pageStyle} title`}>{item.title}</div>
+                <div className="value">{item.value}</div>
+            </div>
+          )
+      })}
+    </div>
+    );
 };
 
 export default AdditionalInfo;
