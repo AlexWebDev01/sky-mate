c18b9c7dea4b0358152ff5b8ba43d927b2fc651a
Alex Udodov <alex.udodov.web@gmail.com>
2023-08-18 23:10:57 +0300

    fix: deleted console.log

--- src/pages/LookPage/LookPage.tsx
+++ src/pages/LookPage/LookPage.tsx
@@ -10,6 +10,7 @@ import "./LookPage.css";
 import { fetchWeatherData } from "../../api/fetchWeatherData";
 import { fetchUserLocatioByIP } from "../../api/fetchUserLocationByIp";
 import { separateCoordinates } from "../../helpers";
+import { calculateClothesAdvice } from "../../clothes-algorithm/clothesAlgorithm";
 
 const LookPage = () => {
   const props = useLocation();
@@ -25,104 +26,105 @@ const LookPage = () => {
   const [location, setLocation] = useState(props.state ? props.state.location : '');
 
   const pageStyle = data.daily[0].weather[0].main.toLowerCase()
+  console.log('WEATHER ADVICE: ', calculateClothesAdvice(data.daily[0].weather[0].main, data.daily[0].temp.day));
 
-    useEffect(() => {
-      if (!props.state) getUserLocation();
-    }, [])
+  useEffect(() => {
+    if (!props.state) getUserLocation();
+  }, []);
 
-    const getUserLocation = () => {
-        fetchUserLocatioByIP()
+  const getUserLocation = () => {
+      fetchUserLocatioByIP()
+      .then((res) => res.json())
+      .then((result) => {
+        const {latitude, longitude}: any = separateCoordinates(result.loc);
+        console.log(latitude, longitude);
+        setLocation(result.city);
+        setLat(latitude);
+        setLon(longitude);
+        setNewDataFetched(true);
+        console.log("USER LOCATION: ", result);
+      })
+      .catch(error => console.error(error));
+    };
+
+  const getWeatherData = () => {
+      fetchWeatherData(lat, lon, "metric")
         .then((res) => res.json())
         .then((result) => {
-          const {latitude, longitude}: any = separateCoordinates(result.loc);
-          console.log(latitude, longitude);
-          setLocation(result.city);
-          setLat(latitude);
-          setLon(longitude);
-          setNewDataFetched(true);
-          console.log("USER LOCATION: ", result);
-        })
-        .catch(error => console.error(error));
-      };
-
-    const getWeatherData = () => {
-        fetchWeatherData(lat, lon, "metric")
-          .then((res) => res.json())
-          .then((result) => {
-            setData(result);
-            setIsLoading(false);
-            console.log("DATA:", result);
-          });
-      };
-
-    const handleSearch = (location: string) => {
-      fetch(
-        `${
-          import.meta.env.VITE_GEOCODING_API_URL
-        }/direct?q=${location}&limit=${1}&appid=${
-          import.meta.env.VITE_WEATHER_APP_API_KEY
-        }`
-      )
-        .then((res) => res.json())
-        .then((result) => {
-          setLat(result[0].lat);
-          setLon(result[0].lon);
-          setNewDataFetched(true);
+          setData(result);
+          setIsLoading(false);
+          console.log("DATA:", result);
         });
     };
 
-    useEffect(() => {
-        if (!lat || !lon) return
-
-        if (newDataFetched) {
-          getWeatherData();
-  
-          fetch(
-            `${
-              import.meta.env.VITE_GEOCODING_API_URL
-            }/reverse?lat=${lat}&lon=${lon}&limit=${1}&appid=${
-              import.meta.env.VITE_WEATHER_APP_API_KEY
-            }`
-          )
-          .then((res) => res.json())
-          .then((result) => {
-            setLocation(result[0].name);
-            console.log("LOCATION: ", result[0]);
-          });
-        }
-
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-      }, [lat, lon]);
-
-    if (isLoading)
-    return (
-      <div className="loader">
-        <img src="./loader.png" />
-      </div>
-    );
-
-    return (
-        <div className='look-page'>
-            <NavBar handleSearch={handleSearch} pageStyle={pageStyle}/>
-              <div className="left-part">
-                <h2 className="location">{location}</h2>
-                <AdditionalInfo weatherData={data} /> 
-                <NavigationLink navigationTo="/" />
-              </div>
-              <div className="right-part">
-                <h1>Protect look</h1>
-                <div className={`${pageStyle} temperature`}>{Math.round(data.current.temp)}&deg; C</div>
-                <ul className="clothes-list">
-                  {
-                    clothesList.map((item: string, index: number) => {
-                      return <li key={index}>{item}</li>
-                    })
-                  }
-                </ul>
-              </div>
-              <p className="clothes-description">{clotherDescription}</p>
-        </div>
+  const handleSearch = (location: string) => {
+    fetch(
+      `${
+        import.meta.env.VITE_GEOCODING_API_URL
+      }/direct?q=${location}&limit=${1}&appid=${
+        import.meta.env.VITE_WEATHER_APP_API_KEY
+      }`
     )
+      .then((res) => res.json())
+      .then((result) => {
+        setLat(result[0].lat);
+        setLon(result[0].lon);
+        setNewDataFetched(true);
+      });
+  };
+
+  useEffect(() => {
+      if (!lat || !lon) return
+
+      if (newDataFetched) {
+        getWeatherData();
+
+        fetch(
+          `${
+            import.meta.env.VITE_GEOCODING_API_URL
+          }/reverse?lat=${lat}&lon=${lon}&limit=${1}&appid=${
+            import.meta.env.VITE_WEATHER_APP_API_KEY
+          }`
+        )
+        .then((res) => res.json())
+        .then((result) => {
+          setLocation(result[0].name);
+          console.log("LOCATION: ", result[0]);
+        });
+      }
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [lat, lon]);
+
+  if (isLoading)
+  return (
+    <div className="loader">
+      <img src="./loader.png" />
+    </div>
+  );
+
+  return (
+      <div className='look-page'>
+          <NavBar handleSearch={handleSearch} pageStyle={pageStyle}/>
+            <div className="left-part">
+              <h2 className="location">{location}</h2>
+              <AdditionalInfo weatherData={data} /> 
+              <NavigationLink navigationTo="/" />
+            </div>
+            <div className="right-part">
+              <h1>Protect look</h1>
+              <div className={`${pageStyle} temperature`}>{Math.round(data.current.temp)}&deg; C</div>
+              <ul className="clothes-list">
+                {
+                  clothesList.map((item: string, index: number) => {
+                    return <li key={index}>{item}</li>
+                  })
+                }
+              </ul>
+            </div>
+            <p className="clothes-description">{clotherDescription}</p>
+      </div>
+  )
 };
 
 export default LookPage;
