09d19657f50eb3ce5e4fa9c234f56c2d1d587b54
Alex Udodov <alex.udodov.web@gmail.com>
2024-02-20 23:13:24 +0200

    feat: introduced global context

---
+++ src/context/GlobalContext.tsx
@@ -0,0 +1,49 @@
+import { useState, createContext, useContext, FunctionComponent } from "react";
+import {
+  Coordinates,
+  GlobalContext,
+  GlobalProviderProps,
+} from "./GlobalContext.interface";
+
+const GlobalContext = createContext<GlobalContext | null>(null);
+
+export const useGlobalContext = () => {
+  const context = useContext(GlobalContext);
+
+  if (!context) {
+    throw new Error("useWords must be used within a WordsProvider");
+  }
+
+  return context;
+};
+
+export const GlobalProvider: FunctionComponent<GlobalProviderProps> = ({
+  children,
+}) => {
+  const [coordinates, setCoordinates] = useState<Coordinates | null>(null);
+  const [weatherData, setWeatherData] = useState<object | null>(null);
+  const [location, setLocation] = useState<string>("");
+  const [expandedCard, setExpandedCard] = useState<
+    "weather-card" | "daily-forecast"
+  >("weather-card");
+  const [pageStyle, setPageStyle] = useState<string>("");
+
+  const sharedState: GlobalContext = {
+    coordinates,
+    setCoordinates,
+    weatherData,
+    setWeatherData,
+    location,
+    setLocation,
+    expandedCard,
+    setExpandedCard,
+    pageStyle,
+    setPageStyle,
+  };
+
+  return (
+    <GlobalContext.Provider value={sharedState}>
+      {children}
+    </GlobalContext.Provider>
+  );
+};
