1afbe83a6bb4765cdc80e3b2958a22ebba8f1ddb
Alex Udodov <alex.udodov.web@gmail.com>
2024-02-22 23:05:42 +0200

    feat: updated LookPage render logic

--- src/pages/LookPage/LookPage.tsx
+++ src/pages/LookPage/LookPage.tsx
@@ -19,51 +19,52 @@ const LookPage = () => {
     fetchData();
   }, [fetchData]);
 
-  const calcResult = calculateClothesAdvice(
-    weatherData.daily[0].weather[0].main,
-    weatherData.daily[0].temp.day
-  );
-  const clothesList = calcResult
-    ? calcResult.adviceData.clothesList
-    : ["Shorts/Skirt", "Rubber Boots", "Umbrella", "Cotton T-Shirt", "Cap"];
-  const clotherDescription = calcResult
-    ? calcResult.adviceData.clothesDescription
-    : "Wear a lightweight, water-resistant jacket made of nylon or polyester to keep you dry in the rain. Layer with a long-sleeved shirt made of a moisture-wicking fabric like polyester or merino wool to keep you comfortable and dry.";
-  const tempRangeName = calcResult
-    ? calcResult.tempRangeName.toLowerCase().replace("_", "-")
-    : "";
-  console.log("TEMP RANGE NAME: ", tempRangeName);
-
-  if (isLoading)
+  if (isLoading || !weatherData) {
     return (
       <div className="loader">
         <img src="./loader.png" />
       </div>
     );
+  } else {
+    const calcResult = calculateClothesAdvice(
+      weatherData.daily[0].weather[0].main,
+      weatherData.daily[0].temp.day
+    );
+    const clothesList = calcResult
+      ? calcResult.adviceData.clothesList
+      : ["Shorts/Skirt", "Rubber Boots", "Umbrella", "Cotton T-Shirt", "Cap"];
+    const clotherDescription = calcResult
+      ? calcResult.adviceData.clothesDescription
+      : "Wear a lightweight, water-resistant jacket made of nylon or polyester to keep you dry in the rain. Layer with a long-sleeved shirt made of a moisture-wicking fabric like polyester or merino wool to keep you comfortable and dry.";
+    const tempRangeName = calcResult
+      ? calcResult.tempRangeName.toLowerCase().replace("_", "-")
+      : "";
+    console.log("TEMP RANGE NAME: ", tempRangeName);
 
-  return (
-    <div className="look-page">
-      <NavBar />
-      <div className="left-part">
-        <h2 className="location">{location}</h2>
-        <AdditionalInfo expanded="weather-card" />
-      </div>
-      <NavigationLink navigationTo="/" />
-      <div className="right-part">
-        <h1>Protect look</h1>
-        <div className={`${pageStyle} temperature`}>
-          {Math.round(weatherData.current.temp)}&deg; C
+    return (
+      <div className="look-page">
+        <NavBar />
+        <div className="left-part">
+          <h2 className="location">{location}</h2>
+          <AdditionalInfo expanded="weather-card" />
         </div>
-        <ul className="clothes-list">
-          {clothesList.map((item: string, index: number) => {
-            return <li key={index}>{item}</li>;
-          })}
-        </ul>
+        <NavigationLink navigationTo="/" />
+        <div className="right-part">
+          <h1>Protect look</h1>
+          <div className={`${pageStyle} temperature`}>
+            {Math.round(weatherData.current.temp)}&deg; C
+          </div>
+          <ul className="clothes-list">
+            {clothesList.map((item: string, index: number) => {
+              return <li key={index}>{item}</li>;
+            })}
+          </ul>
+        </div>
+        <p className="clothes-description">{clotherDescription}</p>
+        <Background page="lookPage" tempRangeName={tempRangeName} />
       </div>
-      <p className="clothes-description">{clotherDescription}</p>
-      <Background page="lookPage" tempRangeName={tempRangeName} />
-    </div>
-  );
+    );
+  }
 };
 
 export default LookPage;
