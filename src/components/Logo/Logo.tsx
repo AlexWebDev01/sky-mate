b3777f4632d7c03b8f6615b7fe6c1f809763003c
Alex Udodov <alex.udodov.web@gmail.com>
2024-03-20 00:27:10 +0200

    feat: enlarged logo

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -6,8 +6,8 @@ const Logo = () => {
   return (
     <Link to="/" className="logo-link">
       <svg
-        width="85"
-        height="22"
+        width="212.5"
+        height="55"
         viewBox="0 0 85 22"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
