95cfa8f8faa6bc48cc58a87a61d1a39d523d6394
Alex Udodov <alex.udodov.web@gmail.com>
2024-04-05 20:40:55 +0300

    fix: changed colors to vars

--- src/pages/MainRouting.tsx
+++ src/pages/MainRouting.tsx
@@ -3,13 +3,13 @@ import { Routes, Route } from "react-router-dom";
 import WeatherPage from "./WeatherPage/WeatherPage";
 import LookPage from "./LookPage/LookPage";
 
-function MainRouting() {
+const MainRouting = () => {
   return (
     <Routes>
       <Route path="/" element={<WeatherPage />} />
       <Route path="/look" element={<LookPage />} />
     </Routes>
   );
-}
+};
 
 export default MainRouting;
