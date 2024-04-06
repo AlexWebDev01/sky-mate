5fcb41ff53e78de11acacc59390957e8ab2ff462
Alex Udodov <alex.udodov.web@gmail.com>
2024-04-06 21:31:23 +0300

    feat: optimized NavBar with memo()

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,15 +1,16 @@
+import { memo } from "react";
 import SearchBar from "../SearchBar/SearchBar";
 import Logo from "../Logo/Logo";
 
 import "./NavBar.css";
 
-const NavBar = () => {
+const NavBar = memo(() => {
   return (
     <div className="nav-bar">
       <Logo />
       <SearchBar />
     </div>
   );
-};
+});
 
 export default NavBar;
