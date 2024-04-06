0541c0c2adb5aa557b18c71b0e224c0889ab4d5c
Alex Udodov <alex.udodov.web@gmail.com>
2024-04-06 21:26:11 +0300

    feat: developed weather API interfaces

--- src/context/GlobalContext.interface.ts
+++ src/context/GlobalContext.interface.ts
@@ -11,7 +11,7 @@ export interface GlobalState {
   isLoading: boolean;
   coordinates: Coordinates | null;
   location: string | null;
-  weatherData: any | null;
+  weatherData: WeatherData | null;
   pageStyle: string;
   clothesAdvice: ClothesAdviceWithTemp | null;
 }
@@ -27,4 +27,70 @@ export interface Coordinates {
 
 export interface LocationData extends Coordinates {
   name: string;
+}
+
+export interface WeatherData {
+  current: CurrentWeatherData;
+  daily: DailyWeatherData[];
+  hourly: HourlyWeatherData[];
+  lat: number;
+  lon: number;
+  timezone: string;
+  timezone_offset: number;
+}
+
+export interface CurrentWeatherData extends BaseWeatherData {
+  sunrise: number;
+  sunset: number;
+  visibility: number;
+  temp: number;
+  feels_like: number;
+}
+
+export interface HourlyWeatherData extends BaseWeatherData {
+  pop: number;
+  wind_gust: number;
+  visibility: number;
+  temp: number;
+  feels_like: number;
+}
+
+export interface DailyWeatherData extends BaseWeatherData {
+  feels_like: { day: number; night: number; eve: number; morn: number };
+  moon_phase: number;
+  moonrise: number;
+  moonset: number;
+  pop: number;
+  rain: number;
+  summary: string;
+  sunrise: number;
+  sunset: number;
+  temp: {
+    day: number;
+    eve: number;
+    max: number;
+    min: number;
+    morn: number;
+    night: number;
+  };
+  wind_gust: number;
+}
+
+interface BaseWeatherData {
+  clouds: number;
+  dew_point: number;
+  dt: number;
+  humidity: number;
+  pressure: number;
+  uvi: number;
+  weather: Weather[];
+  wind_deg: number;
+  wind_speed: number;
+}
+
+export interface Weather {
+  description: string;
+  icon: string;
+  id: number;
+  main: string;
 }
